e73a0a9ed2aa806daf7de8ebbf8e8629becc5c19
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2025-10-25 18:21:58 +0530

    setting

--- smart_campus/settings.py
+++ smart_campus/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hl!yd@nt-zt$yx-ij-ot@rl!zt!hl=gy!hl=gy!hl=gy!hl=gy')
+SECRET_KEY = 'django-insecure-hl!yd@nt-zt$yx-ij-ot@rl!zt!hl=gy!hl=gy!hl=gy!hl=gy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -96,10 +92,10 @@ WSGI_APPLICATION = "smart_campus.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
